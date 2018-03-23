c8e36a39f5785973ad551564c7357c4f614dbc95
Sebasthian von Bergen <sgvonbergen@uc.cl>
2018-03-23 16:40:44 -0300

    Input management on main. Looking at fscanf for file reading

--- T01/src/main.c
+++ T01/src/main.c
@@ -1,5 +1,7 @@
 // Library for input output and file reading
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 // Structures for managing processes and priority queues.
 #include "process.h"
 #include "queue.h"
@@ -7,6 +9,11 @@
 
 int main(int argc, char* argv[])
 {
-  // code here
+  // if version parameter exists
+  if (argc >= 2) {
+    if (strcmp(argv[1], "v1") == 0) {
+
+    }
+  }
   return 0;
 }
