6316cb54cc0dcfca15e4c9c13628462b5b9f44b0
Sebasthian von Bergen <sgvonbergen@uc.cl>
2018-05-30 12:56:18 -0400

    Setting up listener in server

--- T04/Client/src/Client/main.c
+++ T04/Client/src/Client/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
+#include <sys/socket.h>
 
 void signal_handler(int sig){
   if (sig == SIGINT){
@@ -13,10 +14,27 @@ void signal_handler(int sig){
 int main(int argc, char* argv[])
 {
   signal(SIGINT,signal_handler);
-  if (argc != 2) {
-    printf("Incorrect parameters, to run the program do:\n/czfx <disk>\nwhere <disk> is the path to the file that simulates the virtual disk.\n");
+  if (argc != 5) {
+    printf("Incorrect parameters, to run the program do:\n$ ./client -i <ip_address> -p <tcp-port>\n");
     return 1;
   }
+
+  int ip, port;
+  for (size_t i = 0; i < 5; i++) {
+    if (strcmp(argv[i], "-i")) {
+      ip = atoi(argv[i+1]);
+    }
+    if (strcmp(argv[i], "-p")) {
+      port = atoi(argv[i+1]);
+    }
+  }
+
+  // Attempts to create a socket until it is succesful
+  int client_socket = -1;
+  do {
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+  } while(client_socket != -1);
+
   printf("%s\n", "hihi");
   return 0;
 }
--- T04/Server/src/Server/main.c
+++ T04/Server/src/Server/main.c
@@ -2,10 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+int MAX_WAITING_CONNECTIONS = 5;
 
 void signal_handler(int sig){
   if (sig == SIGINT){
-    // do stuff
+    // close all connections before exiting
     exit(0);
   }
 }
@@ -13,10 +18,39 @@ void signal_handler(int sig){
 int main(int argc, char* argv[])
 {
   signal(SIGINT,signal_handler);
-  if (argc != 2) {
-    printf("Incorrect parameters, to run the program do:\n/czfx <disk>\nwhere <disk> is the path to the file that simulates the virtual disk.\n");
+  if (argc != 5) {
+    printf("Incorrect parameters, to run the program do:\n$ ./server -i <ip_address> -p <tcp-port>\n");
     return 1;
   }
-  printf("%s\n", "hihi");
+
+  int port;
+  char *ip;
+  for (size_t i = 0; i < 5; i++) {
+    if (strcmp(argv[i], "-i")) {
+      ip = argv[i+1];
+    }
+    if (strcmp(argv[i], "-p")) {
+      port = atoi(argv[i+1]);
+    }
+  }
+
+  // Creating Listener Socket for receiving new connection attempts
+  struct sockaddr_in server;
+  int listener = -1;
+  do {
+    listener = socket(AF_INET, SOCK_STREAM, 0);
+  } while(listener != -1);
+
+  server.sin_addr.s_addr = inet_addr(ip);
+  server.sin_family = AF_INET;
+  server.sin_port= htons(port);
+
+  // Attempting to bind the listener to the designated port
+  int bind_success = -1;
+  do {
+    bind_success = bind(listener, (struct sockaddr*)&server, sizeof(server));
+  } while(bind_success != 0);
+  listen(listener, MAX_WAITING_CONNECTIONS);
+
   return 0;
 }
